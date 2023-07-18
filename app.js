ca812396efa1e9d005c745d49673e8688c61bd57
Abel Kifletsion <fishabel8@gmail.com>
2023-07-18 16:31:44 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+
+const userRouter = require('./routers/userRouter');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 80;
+const cors = require('cors');
+const { checkToken } = require('./middlewares/checkToken');
+const app = express();
+const mongoose = require('mongoose');
+const { DB_SERVER } = require('./config.json');
+const noteRouter = require('./routers/noteRouter');
+
+// Mongoose
+// connect to database
+mongoose
+  .connect(DB_SERVER
+  )
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Logger
+app.use(morgan('dev'));
+// CORS
+app.use(cors());
+
+
+// Body parser
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api/users', userRouter);
+// images
+app.use('/api/notes/images', express.static('uploads'));
+// NoteRoutes here
+app.use('/api/notes', checkToken, noteRouter);
+//health
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
+// Error handling
+app.all((req, res, next) => {
+  next(new Error(`Can't find ${req.originalUrl} on this server!`));
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
